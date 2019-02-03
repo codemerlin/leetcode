fae5f5a7ef0b1c65bfd997339d65885e1f9a04ec
Mohit Thakral <me@mohitt.com>
2019-02-02 17:02:38 -0800

    recursive implementation

--- 10.regular-expression-matching.cs
+++ 10.regular-expression-matching.cs
@@ -91,7 +91,7 @@ public class Solution {
     // basically a pattern matches if substings match
     private bool IsMatchRecursive(string s, string p) {
         if (string.IsNullOrEmpty(p)) return string.IsNullOrEmpty(s);
-        var firstMatch = (!s.IsNullOrEmpty() && 
+        var firstMatch = (!string.IsNullOrEmpty(s) && 
                         (p[0] == s[0] || p[0] == '.'));
         if(p.Length >=2 && p[1] == '*') {
             return this.IsMatchRecursive(s,p.Substring(2)) 
