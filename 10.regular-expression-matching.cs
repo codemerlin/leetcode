d4665b8ff577f4c1848db5e33cd2238843478c4b
Mohit Thakral <me@mohitt.com>
2019-02-02 18:26:17 -0800

    dp implementation

--- 10.regular-expression-matching.cs
+++ 10.regular-expression-matching.cs
@@ -89,17 +89,17 @@ public class Solution {
     }
     
     private bool IsMatchDp(string s, string p) {
-      var dp = new bool[s.Length+1][p.Length+1];
-      dp[s.Length][p.Length] = true;
+      var dp = new bool[s.Length+1,p.Length+1];
+      dp[s.Length,p.Length] = true;
 
       for(int i=s.Length; i>=0; i--) {
           for(int j=p.Length; j>=0; j--) {
               var firstMatch = i < s.Length && 
                                 (p[j] == s[i] || p[i] =='.');
               if(j=1 < p.Length && p[j+1] == '*') {
-                  dp[i][j] = dp[i][j+2] || firstMatch && dp[i+1][j];
+                  dp[i,j] = dp[i,j+2] || firstMatch && dp[i+1,j];
               } else {
-                  dp[i][j] = firstMatch && dp[i+1][j+1];
+                  dp[i,j] = firstMatch && dp[i+1,j+1];
               } 
 
           }
